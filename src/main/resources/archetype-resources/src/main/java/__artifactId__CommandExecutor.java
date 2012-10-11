e1105db0c6ab26dfc24102c65912da189520647b
robineaj <robineaj@zirconia.cs.unca.edu>
2012-10-10 20:49:22 -0400

    added code to plugin.yml

--- src/main/resources/archetype-resources/src/main/java/__artifactId__CommandExecutor.java
+++ src/main/resources/archetype-resources/src/main/java/__artifactId__CommandExecutor.java
@@ -37,11 +37,29 @@ public class ${artifactId}CommandExecutor implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         plugin.getLogger().info("onCommand Reached in ${artifactId}");
 
-        if (command.getName().equalsIgnoreCase("command")) {
-	    plugin.getLogger().info("command used");
-            //do something
-            return true;
-        }
-        return false;
+        if (args.length == 0) {
+        	return false;
+        	} else if (!(sender instanceof Player)) {
+        	return false;
+        	// the cake will appear on the ground but not
+        	// necessarily where the player is looking
+        	} else if (args[0].equalsIgnoreCase("cake")) {
+        	Player fred = (Player) sender;
+        	Location loc = fred.getLocation();
+        	World w = loc.getWorld();
+        	loc.setX(loc.getX() + 1);
+        	Block b = w.getBlockAt(loc);
+        	b.setTypeId(92);
+        	return true;
+        	// the stored message now always begins with
+        	// the word "message"--do you know how to easily
+        	// fix that problem?
+        	} else if (args[0].equalsIgnoreCase("message")
+        	&& sender.hasPermission("plugin1.message")) {
+        	this.plugin.getConfig().set("sample.message",
+        	Joiner.on(' ').join(args));
+        	return true;
+        	} else {
+        	return false;
     }
 }
